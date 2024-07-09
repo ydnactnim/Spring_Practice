9af681d1cab092ef1bb46bd45ba980a281404723
ydnactnim <ydnactnim@gmail.com>
2024-07-10 03:15:25 +0900

    Spring Init
    Spring Init

---
+++ Spring_Practice/src/test/java/JSK/Spring_Practice/SpringPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package JSK.Spring_Practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
